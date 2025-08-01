0353c756256aa2de5bb82d8997cd4698b183b777
pt-nakul-sharma <nakulsharma.1296@gmail.com>
2025-08-01 20:30:48 +0530

    chore: initialize Rails project with default configuration and cache files

---
+++ app/jobs/sample_job.rb
@@ -0,0 +1,12 @@
+class SampleJob < ApplicationJob
+  queue_as :default
+
+  def perform(name)
+    Rails.logger.info "Hello, #{name}! This job was processed by SolidQueue at #{Time.current}"
+    
+    # Simulate some work
+    sleep 2
+    
+    Rails.logger.info "Job completed for #{name}"
+  end
+end
